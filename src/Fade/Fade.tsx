8b1ad177042f45eae4729c086d7c1907cf8ed9bf
Pontus Alexander <pontus.alexander@gmail.com>
2022-12-11 18:16:09 +0100

    No material UI dep

--- src/Fade/Fade.tsx
+++ src/Fade/Fade.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
 import { TransitionEffectProps } from "../types";
 
-function opacity({ active }: Omit<TransitionEffectProps, "children">) {
-  return active ? 1 : 0;
-}
-
-const useStyles = makeStyles({
-  Fade: {
-    height: "100%",
-    opacity,
-    pointerEvents: (props) => (props.active ? "all" : "none"),
-    transition: "opacity 0.5s ease",
-  },
-});
-
 export default function Fade({ active, children }: TransitionEffectProps) {
-  const classes = useStyles({ active });
-  return <div className={classes.Fade}>{children}</div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        opacity: active ? 1 : 0,
+        pointerEvents: active ? "all" : "none",
+        transition: "opacity 0.5s ease",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Direction } from "./direction";
 import Fade from "./Fade";
 import Slide from "./Slide";
-import SlidingWindow from "./SlidingWindow";
 import Zoom from "./Zoom";
 
-export { Fade, Slide, Direction, SlidingWindow, Zoom };
+export { Fade, Slide, Direction, Zoom };
